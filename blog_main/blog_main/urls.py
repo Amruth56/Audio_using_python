0a35b9b440f2fc7383682cf58c36e963d21d9f70
Amruth <ammumandappa@gmail.com>
2026-01-11 20:54:01 +0530

    jinja 2 and django apps

--- blog_main/blog_main/urls.py
+++ blog_main/blog_main/urls.py
@@ -12,11 +12,16 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls')) 
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('chai/', include('chai.urls')),
 ]
---
+++ blog_main/blog_main/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    # return HttpResponse("Hello World")
+    return render(request, 'index.html')
+
+def about(request):
+    return HttpResponse("This is About Page")
+    
+def contact(request):
+    return HttpResponse("This is Contact Page") 
